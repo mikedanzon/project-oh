b8255d533d1f0a7f9a5936ff1ff07e074091d683
Michael Max <mikedanzon@gmail.com>
2021-04-11 20:59:01 +0700

    fix padding

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Home from './pages/Home';
 import PortoTwo from './pages/porto/PortoTwo';
 
 function App() {
-  return (
-    <>
-      <Switch>
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/porto2' component={PortoTwo}/>
-      </Switch>
-    </>
-  );
+    return (
+        <>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/porto2" component={PortoTwo} />
+            </Switch>
+        </>
+    );
 }
 
 export default App;
